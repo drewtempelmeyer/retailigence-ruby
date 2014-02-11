c1576e8e30ae3fe938665ec2cffc35b971525f7a
Drew Tempelmeyer <drewtemp@gmail.com>
2014-02-10 16:42:16 -0800

    Change cassette directory to test/cassettes

--- test/vcr_setup.rb
+++ test/vcr_setup.rb
@@ -1,7 +1,7 @@
 require 'vcr'
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'cassettes'
+  c.cassette_library_dir = 'test/cassettes'
   c.hook_into :typhoeus
   c.filter_sensitive_data('<API_KEY>') { ENV['RETAILIGENCE_API_KEY'] || 'retailigence_api_key' }
 end
