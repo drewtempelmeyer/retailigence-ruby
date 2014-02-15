414f1d719ff4bcfade22b9304798e047a62bf3a7
Drew Tempelmeyer <drewtemp@gmail.com>
2014-02-14 21:29:17 -0800

    Update exceptions

--- lib/retailigence/exceptions.rb
+++ lib/retailigence/exceptions.rb
@@ -1,5 +1,7 @@
 module Retailigence #:nodoc:
-  # Raised when Retailigence has not yet be configured.
-  # See Retailigence::configure.
-  class NotConfigured < StandardError; end
+  # Raised when Retailigence doesn't find any results
+  class NoResults < StandardError; end
+
+  # Raised when any other exception occurs from the API
+  class APIException < StandardError; end
 end
