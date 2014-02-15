4c0924b464f8c6a5a3723e911d5425860f3e4067
Drew Tempelmeyer <drewtemp@gmail.com>
2014-02-14 21:28:00 -0800

    Change cassette options

--- test/vcr_setup.rb
+++ test/vcr_setup.rb
@@ -3,5 +3,8 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'test/cassettes'
   c.hook_into :typhoeus
+  c.default_cassette_options = {
+    match_requests_on: [ :method, :path ]
+  }
   c.filter_sensitive_data('<API_KEY>') { ENV['RETAILIGENCE_API_KEY'] || 'retailigence_api_key' }
 end
